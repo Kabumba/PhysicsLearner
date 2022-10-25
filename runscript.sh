1a86ed5e07f9738b4e3b9d9867ed3394eabb6b33
Kabumba <Freddy253@live.de>
2022-10-25 14:03:41 +0200

    test runscript

---
+++ runscript.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH -C cgpu01
+#SBATCH -c 20
+#SBATCH --mem=6G
+#SBATCH --gres=gpu:2
+#SBATCH --partition=short
+#SBATCH --time=00:05:00
+#SBATCH --job-name=hallo
+#SBATCH --output=/work/smfrrohk/Masterarbeit/hallo.log
+#SBATCH --mail-user=frederik.rohkraehmer@tu-dortmund.de
+#SBATCH --mail-type=ALL
+#-------------------------------------
+
+module purge
+module load nvidia/cuda/11.1.1
+module load gcc/11.1.0
+
+source /home/smfrrohk/anaconda3/bin/activate
+conda activate PhysicsLearner
+
+cd /work/smfrrohk/Masterarbeit/PhysicsLearner
+srun python -m main.py
