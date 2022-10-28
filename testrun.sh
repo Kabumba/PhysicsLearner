6e15f0191783ffcb09e8d14eeb08514206772828
Kabumba <Freddy253@live.de>
2022-10-28 12:36:08 +0200

    runscripts

--- testrun.sh
+++ testrun.sh
@@ -15,7 +15,7 @@
 module purge
 module load nvidia/cuda/11.1.1
 
-source /home/smfrrohk/anaconda3/bin/activate
+source /home/smfrrohk/anaconda3/bin
 conda activate PhysicsLearner
 
 cd /work/smfrrohk/Masterarbeit/PhysicsLearner
