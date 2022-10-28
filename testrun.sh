63f48bb7eb6930b071cc9e9f525368b711463e01
Kabumba <Freddy253@live.de>
2022-10-28 10:22:40 +0200

    runscripts

--- testrun.sh
+++ testrun.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 #SBATCH -C cgpu01
-#SBATCH -c 20
-#SBATCH --mem=60G
+#SBATCH -c 1
+#SBATCH --mem=1G
 #SBATCH --gres=gpu:1
 #SBATCH --partition=short
-#SBATCH --time=02:00:00
+#SBATCH --time=00:00:20
 #SBATCH --job-name=test
 #SBATCH --output=/work/smfrrohk/Masterarbeit/logs/test.log
 #SBATCH --mail-user=frederik.rohkraehmer@tu-dortmund.de
@@ -13,7 +13,6 @@
 
 module purge
 module load nvidia/cuda/11.1.1
-module load gcc/11.1.0
 
 source /home/smfrrohk/anaconda3/bin/activate
 conda activate PhysicsLearner
