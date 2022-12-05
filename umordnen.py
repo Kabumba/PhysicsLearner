ba109618670fb4361ea7bf599acd6e999b2ab6d5
Kabumba <Freddy253@live.de>
2022-12-05 23:06:45 +0100

    umordnen

--- umordnen.py
+++ umordnen.py
@@ -2,18 +2,23 @@ import os
 import shutil
 from IO_manager import Directories
 
-
 dirs = Directories()
 state_dir = dirs.BASE_DIR + "/Train"
-trash_dir = dirs.BASE_DIR + "/Unused"
+trash_dir = dirs.BASE_DIR + "/Unused2"
 gs = os.listdir(state_dir)
 upsi_files = []
 for f in gs:
     base_name = os.path.splitext(os.path.basename(f))[0]
     if f.startswith("[GaussSteps"):
         shutil.move(os.path.join(state_dir, f), os.path.join(trash_dir, f))
-    if f.startswith("[ConstantSplit,0."):
+    if f.startswith("[ConstantSplit,0"):
         shutil.move(os.path.join(state_dir, f), os.path.join(trash_dir, f))
     if f.startswith("[Interpolator"):
+        shutil.move(os.path.join(state_dir, f), os.path.join(trash_dir, f))
+    if f.startswith("[Smooth"):
+        shutil.move(os.path.join(state_dir, f), os.path.join(trash_dir, f))
+    if f.startswith("[Directed"):
+        shutil.move(os.path.join(state_dir, f), os.path.join(trash_dir, f))
+    if f.startswith("[Randomizer"):
         shutil.move(os.path.join(state_dir, f), os.path.join(trash_dir, f))
 print(f"done {len(upsi_files)}")
