2dfa65aee26694278ea17a43380410e40e97c1cb
Kabumba <Freddy253@live.de>
2022-12-05 23:20:56 +0100

    med

--- med0.sh
+++ med0.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -l
-#SBATCH -c 1
-#SBATCH --mem 8G
-#SBATCH --partition=short
-#SBATCH --time=00:20:00
-#SBATCH --job-name=Files
+#SBATCH -c 10
+#SBATCH --mem 30G
+#SBATCH --gres=gpu:1
+#SBATCH --partition=med
+#SBATCH --time=08:00:00
+#SBATCH --job-name=Med0
 #SBATCH --output=/work/smfrrohk/Masterarbeit/Logs/run-%j.log
 #SBATCH --mail-user=frederik.rohkraehmer@tu-dortmund.de
 #SBATCH --mail-type=ALL
@@ -17,4 +18,4 @@ source /home/smfrrohk/anaconda3/bin/activate
 conda activate PhysicsLearner
 
 cd /work/smfrrohk/Masterarbeit/PhysicsLearner
-srun python umordnen.py
+srun python main.py --name 0
